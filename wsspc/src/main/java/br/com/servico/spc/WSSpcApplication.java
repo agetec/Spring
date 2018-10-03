be3423ad81061e0c164762dc09f21225f5f2fcbc
agetec <lucasdossantosdesouza@gmail.com>
2018-10-03 15:27:24 -0400

    --

--- wsspc/src/main/java/br/com/servico/spc/WSSpcApplication.java
+++ wsspc/src/main/java/br/com/servico/spc/WSSpcApplication.java
@@ -2,9 +2,15 @@ package br.com.servico.spc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WSSpcApplication {
+public class WSSpcApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WSSpcApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WSSpcApplication.class, args);
