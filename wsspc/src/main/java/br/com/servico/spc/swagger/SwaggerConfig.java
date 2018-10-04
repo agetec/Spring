08df3bbc0dd61003a46602e1e59f21cee8d0a160
agetec <lucasdossantosdesouza@gmail.com>
2018-10-04 08:42:03 -0400

    --

--- wsspc/src/main/java/br/com/servico/spc/swagger/SwaggerConfig.java
+++ wsspc/src/main/java/br/com/servico/spc/swagger/SwaggerConfig.java
@@ -29,10 +29,10 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new ApiInfoBuilder()
                 .title("Spring Boot REST API")
                 .description("\"Spring Boot REST API for Online Store\"")
-                .version("1.0.0")
+                .version("2.0.0")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .contact(new Contact("", "https://springframework.guru/about/", ""))
                 .build();
     }
     @Override
