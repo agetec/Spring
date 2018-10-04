3b27285d49ba78b3a742fcc626fe4a18dbd84d1a
agetec <lucasdossantosdesouza@gmail.com>
2018-10-04 16:22:33 -0400

    --

--- wsspc/src/main/java/br/com/servico/spc/sconfig/SwaggerConfig.java
+++ wsspc/src/main/java/br/com/servico/spc/sconfig/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package br.com.servico.spc.sconfig;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,10 +15,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
+@EnableAutoConfiguration 
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 	@Bean
 	public Docket productApi() {
