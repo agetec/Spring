5515332be1678fddb4fb8ff9adef746dfeffeac1
agetec <lucasdossantosdesouza@gmail.com>
2018-10-16 08:32:09 -0400

    --

--- wsspc/src/main/java/br/com/servico/spc/controler/SpcControler.java
+++ wsspc/src/main/java/br/com/servico/spc/controler/SpcControler.java
@@ -1,7 +1,5 @@
 package br.com.servico.spc.controler;
 
-import javax.xml.soap.SOAPMessage;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.servico.spc.model.Operador;
+import br.com.servico.spc.model.Spc;
 import br.com.servico.spc.service.SpcService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -28,13 +27,15 @@ public class SpcControler {
 	@ApiOperation(value = "Incluir inadimplênte no SPC", response = JSONObject.class)
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successo na requisição, com seguinte retorno"),
 			@ApiResponse(code = 404, message = "O recurso que você estava tentando acessar não foi encontrado") })
-	@RequestMapping(method = RequestMethod.POST, value = "/incluirSpc", consumes = MediaType.APPLICATION_JSON_VALUE, 
-					produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.POST, value = "/incluirSpc", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 
 	public ResponseEntity<JSONObject> incluir(@RequestBody Operador operador) {
 		JSONObject message = null;
 		if (operador != null && operador.getSpcs() != null) {
 			message = new SoapSpcControler().callSoapWebServiceInclusao(operador.getSpcs(), operador);
+			for (Spc spc2 : operador.getSpcs()) {
+				spcService.salvar(spc2);
+			}
 
 		} else
 			return new ResponseEntity<JSONObject>(message, HttpStatus.NOT_FOUND);
@@ -45,14 +46,15 @@ public class SpcControler {
 	@ApiOperation(value = "Excluir inadimplênte do SPC", response = JSONObject.class)
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successo na requisição, com seguinte retorno"),
 			@ApiResponse(code = 404, message = "O recurso que você estava tentando acessar não foi encontrado") })
-	@RequestMapping(method = RequestMethod.POST, value = "/excluirSpc", consumes = MediaType.APPLICATION_JSON_VALUE,
-					produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.POST, value = "/excluirSpc", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 
 	public ResponseEntity<JSONObject> excluir(@RequestBody Operador operador) {
 		JSONObject message = null;
 		if (operador != null && operador.getSpcs() != null) {
 			message = new SoapSpcControler().callSoapWebServiceExclusao(operador.getSpcs(), operador);
-
+			for (Spc spc2 : operador.getSpcs()) {
+				spcService.salvar(spc2);
+			}
 		} else
 			return new ResponseEntity<JSONObject>(message, HttpStatus.NOT_FOUND);
 
