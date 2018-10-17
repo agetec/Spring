06962c149cb9812a6de89143d725a3c7bfbc9c69
agetec <lucasdossantosdesouza@gmail.com>
2018-10-17 09:15:35 -0400

    --

--- estados/src/main/java/com/estados/br/repository/EstadosRepository.java
+++ estados/src/main/java/com/estados/br/repository/EstadosRepository.java
@@ -1,9 +1,10 @@
 package com.estados.br.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.estados.br.model.Estados;
 
+@Repository
 public interface EstadosRepository extends JpaRepository<Estados, Integer> {
 
 }
