58ce4f5bce6a3815c4fddd59a217e43dac838917
agetec <lucasdossantosdesouza@gmail.com>
2018-10-17 09:14:42 -0400

    --

--- estados/src/main/java/com/estados/br/controler/EstadosControler.java
+++ estados/src/main/java/com/estados/br/controler/EstadosControler.java
@@ -1,6 +1,5 @@
 package com.estados.br.controler;
 
-import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,7 @@ public class EstadosControler {
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarEstado", 
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<JSONObject> salvar(@RequestBody Estados estado) {
+	public ResponseEntity<Estados> salvar(@RequestBody Estados estado) {
 		return null;
 	}
 	
@@ -27,14 +26,14 @@ public class EstadosControler {
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/buscarAllEstados", 
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<JSONObject> buscarTodos() {
+	public ResponseEntity<Estados> buscarTodos() {
 		return null;		
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/buscarAllEstados",
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<JSONObject> buscarPorId(Integer Id) {
+	public ResponseEntity<Estados> buscarPorId(Integer Id) {
 		return null;		
 	}
 
