4b73f0095bf265f54065ca01b4856faf6cc7c1cd
agetec <lucasdossantosdesouza@gmail.com>
2018-10-23 16:09:43 -0400

    --

--- wsbebidas/src/main/java/com/bebidas/br/util/JsonDateSerializer.java
+++ wsbebidas/src/main/java/com/bebidas/br/util/JsonDateSerializer.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 @Component
 public class JsonDateSerializer extends JsonSerializer<Date> {
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
 	@Override
 	public void serialize(Date date, JsonGenerator gen, SerializerProvider provider)
