85cf287e8e4b809442a0dc47ae15320a3d3f2414
agetec <lucasdossantosdesouza@gmail.com>
2018-10-25 15:01:57 -0400

    --

--- wsbebidas/src/test/java/com/bebidas/br/WsbebidasApplicationTests.java
+++ wsbebidas/src/test/java/com/bebidas/br/WsbebidasApplicationTests.java
@@ -7,9 +7,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-
 import javax.ws.rs.core.MediaType;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +19,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import com.bebidas.br.model.Bebida;
 import com.bebidas.br.model.Estoque;
 import com.bebidas.br.model.HistoricoBebida;
