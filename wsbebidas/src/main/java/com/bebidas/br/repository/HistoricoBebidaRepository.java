b38cd1bb67502ad56084f564d52a844da9314182
agetec <lucasdossantosdesouza@gmail.com>
2018-10-25 17:47:31 -0400

    --

--- wsbebidas/src/main/java/com/bebidas/br/repository/HistoricoBebidaRepository.java
+++ wsbebidas/src/main/java/com/bebidas/br/repository/HistoricoBebidaRepository.java
@@ -9,8 +9,8 @@ import com.bebidas.br.model.HistoricoBebida;
 
 public interface HistoricoBebidaRepository extends JpaRepository<HistoricoBebida, Long> {
 
-	@Query("SELECT  h FROM  HistoricoBebida  h where " + "INNER JOIN Estoque e on e.idEsqtoque=h.estoque.idEstoque"
-			+ "INNER JOIN Bebida b on b.idBebida=e.bebida.idBebida" + "e.sessao.idSessao=:sessao and"
+	@Query("SELECT  h FROM  HistoricoBebida  h " + "INNER JOIN Estoque e on e.idEstoque=h.estoque.idEstoque"
+			+ "INNER JOIN Bebida b on b.idBebida=e.bebida.idBebida" + "where e.sessao.idSessao=:sessao and"
 			+ "b.tipoBebida.idTipoBebida = :tipo")
 	Collection<HistoricoBebida> buscarByTipoSessao(@Param("sessao") Integer sessao, @Param("tipo") Integer tipo);
 }
