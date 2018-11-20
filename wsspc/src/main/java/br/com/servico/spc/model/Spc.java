f0e22f3d1c9ea0538af202fb2b02145a77d3a7f2
agetec <lucasdossantosdesouza@gmail.com>
2018-11-20 15:07:11 -0300

    --

--- wsspc/src/main/java/br/com/servico/spc/model/Spc.java
+++ wsspc/src/main/java/br/com/servico/spc/model/Spc.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -88,6 +89,7 @@ public class Spc {
 	private Integer idExclusao;
 	
 	@ManyToOne(cascade=CascadeType.ALL)
+	@JoinColumn(nullable = false)
 	private Operador spcoperador;
 	
 	@Column(nullable=false)
